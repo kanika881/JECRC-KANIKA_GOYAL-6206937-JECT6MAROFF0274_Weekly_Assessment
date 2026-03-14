95a5d3eef41ad959becbbab943e632f8b6bc8582
kanika881 <Kanikagoyal451@gmail.com>
2026-03-14 13:45:27 +0530

    Delete 12.03.2026 directory

--- 12.03.2026/firefox_open.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-opts=webdriver.FirefoxOptions()
-opts.set_preference("detach", True)
-driver=webdriver.Firefox(options=opts)
-driver.get("https://www.youtube.com")
-driver.maximize_window()
